4705be046420c3a8be5079022e16e54a7d7acdf3
TehZX <s10221851@connect.np.edu.sg>
2022-06-25 10:53:40 +0800

    Ver 1.0

--- app/src/main/java/sg/edu/np/mad/mad_assignment/BlockDetails.java
+++ app/src/main/java/sg/edu/np/mad/mad_assignment/BlockDetails.java
@@ -46,33 +46,32 @@ public class BlockDetails extends AppCompatActivity {
         BlkNo.setText(BlockNumber);
         BlkDescr.setText(BlockDescription);
 
-        RecyclerView name = findViewById(R.id.recyclerVIEW);
-        name.setLayoutManager(new LinearLayoutManager(this));
-        String imgname_1 = blk_info.getName();
-        String imgname_2 = imgname_1 + "_pic";
+//        RecyclerView name = findViewById(R.id.recyclerVIEW);
+//        name.setLayoutManager(new LinearLayoutManager(this));
+//        String imgname_1 = blk_info.getName();
+//        String imgname_2 = imgname_1 + "_pic";
+//
+//        ArrayList<String> data = new ArrayList<>();
+//        data.add(imgname_1);
+//        data.add(imgname_2);
+//
+//        myImageAdapter imgAdp = new myImageAdapter(data);
+//        name.setAdapter(imgAdp);
 
-        ArrayList<String> data = new ArrayList<>();
-        data.add(imgname_1);
-        data.add(imgname_2);
+        //Image of block location
+        ImageView blkIMG = findViewById(R.id.blockMap);
 
-        myImageAdapter imgAdp = new myImageAdapter(data);
-        name.setAdapter(imgAdp);
+        String blk_name = BlockName.replace(" ", "_").toLowerCase();
 
-//        //Image of block location
-//        ImageView blkIMG = findViewById(R.id.blkImage);
-//
-//        String blk_name = BlockName.replace(" ", "_").toLowerCase();
-//
-//        String img_name = blk_name + "_map";
-//
-//        int drawable = getDrawable(this, img_name);
-//
-//        blkIMG.setImageURI(Uri.parse("android.resource://" + getPackageName() + "/" + drawable));
-//
-//    }
-//
-//    public static int getDrawable (Context context, String name){
-//        return context.getResources().getIdentifier(name, "drawable", context.getPackageName());
-//    }
+        String img_name = blk_name + "_map";
+
+        int drawable = getDrawable(this, img_name);
+
+        blkIMG.setImageURI(Uri.parse("android.resource://" + getPackageName() + "/" + drawable));
+
+    }
+
+    public static int getDrawable (Context context, String name){
+        return context.getResources().getIdentifier(name, "drawable", context.getPackageName());
     }
 }
