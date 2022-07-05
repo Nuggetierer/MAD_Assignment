5a84b2aaefaf684743eea8b314e8d652602b7a95
Nathan Quek Xiu Han <s10223513@connect.np.edu.sg>
2022-07-05 17:46:11 +0800

    Update gradle and google-services.json

--- app/src/main/java/sg/edu/np/mad/mad_assignment/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_assignment/MainActivity.java
@@ -6,11 +6,23 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
--- app/src/main/java/sg/edu/np/mad/mad_assignment/ui/Study/StudyFragment.java
+++ app/src/main/java/sg/edu/np/mad/mad_assignment/ui/Study/StudyFragment.java
@@ -1,25 +1,81 @@
 package sg.edu.np.mad.mad_assignment.ui.Study;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import sg.edu.np.mad.mad_assignment.databinding.FragmentStudyBinding;
 
 public class StudyFragment extends Fragment {
 
     private FragmentStudyBinding binding;
+    DatabaseReference databaseReference;
+    public String TAG = "Main Acitivty: ";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         StudyViewModel studyViewModel =
                 new ViewModelProvider(this).get(StudyViewModel.class);
+
+        databaseReference= FirebaseDatabase.getInstance().getReference("this is the path");
+
+        databaseReference.setValue("hello there").addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused){
+                Toast.makeText(getActivity().getApplicationContext(),"success",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener(){
+            @Override
+            public void onFailure(@NonNull Exception e){
+                Toast.makeText(getActivity().getApplicationContext(),"failed",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Void>(){
+            @Override
+            public void onComplete(@NonNull Task<Void> task){
+                //
+            }
+        });
+
+//        // Write a message to the database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        myRef.setValue("Hello, World!");
+
+        // Read from the database
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
 
         binding = FragmentStudyBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
