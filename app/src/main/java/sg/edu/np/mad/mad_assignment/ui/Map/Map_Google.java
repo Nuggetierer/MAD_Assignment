d376ed60f3e920fafebb0807ba7cff02d8ce4e0d
Shanthu2004 <103989017+Shanthu2004@users.noreply.github.com>
2022-07-07 14:19:07 +0800

    Changed the map background colour

---
+++ app/src/main/java/sg/edu/np/mad/mad_assignment/ui/Map/Map_Google.java
@@ -0,0 +1,308 @@
+package sg.edu.np.mad.mad_assignment.ui.Map;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.PersistableBundle;
+import android.provider.Settings;
+import android.telecom.ConnectionRequest;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.IGoogleMapDelegate;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import sg.edu.np.mad.mad_assignment.R;
+
+public class Map_Google extends AppCompatActivity implements OnMapReadyCallback {
+    boolean isPermissionGranter;
+
+    GoogleMap googleMap;
+    ImageView imageViewSearch;
+    EditText inputLocation;
+    Menu menu;
+    LocationRequest locationRequest;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_google);
+
+        imageViewSearch = (ImageView) findViewById(R.id.imageViewSearch);
+        inputLocation = (EditText) findViewById(R.id.inputlocation);
+
+        checkPermission();
+
+
+        if (checkGooglePlayService()) {
+            SupportMapFragment supportMapFragment = SupportMapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, supportMapFragment).commit();
+            supportMapFragment.getMapAsync(this);
+            if (isPermissionGranter) {
+                CheckGPS();
+            }
+
+        } else {
+            Toast.makeText(this, "Google Playservices Not Available", Toast.LENGTH_SHORT).show();
+        }
+
+        imageViewSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Location = inputLocation.getText().toString();
+                if (Location == null) {
+                    Toast.makeText(Map_Google.this, "Type any location name", Toast.LENGTH_SHORT).show();
+                } else {
+                    Geocoder geocoder = new Geocoder(Map_Google.this, Locale.getDefault());
+                    try {
+                        List<Address> listAddress = geocoder.getFromLocationName(Location, 1);
+                        if (listAddress.size() > 0) {
+                            googleMap = googleMap;
+                            LatLng latLng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
+
+                            MarkerOptions markerOptions = new MarkerOptions();
+                            markerOptions.title("Search Location");
+                            markerOptions.position(latLng);
+                            googleMap.addMarker(markerOptions);
+                            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
+                            googleMap.animateCamera(cameraUpdate);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean checkGooglePlayService() {
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int result = googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if (result == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (googleApiAvailability.isUserResolvableError(result)) {
+            Dialog dialog = googleApiAvailability.getErrorDialog(this, result, 201, new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialogInterface) {
+                    Toast.makeText(Map_Google.this, "User Cancelled Dialoge", Toast.LENGTH_SHORT).show();
+                }
+            });
+            dialog.show();
+        }
+
+        return false;
+
+    }
+
+    private void checkPermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                isPermissionGranter = true;
+                Toast.makeText(Map_Google.this, "permission Granter", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), "");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        LatLng latLng = new LatLng(1.3321, 103.7743);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("Ngee Ann Polytechnic");
+        markerOptions.position(latLng);
+        googleMap.addMarker(markerOptions);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
+        googleMap.animateCamera(cameraUpdate);
+
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setCompassEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setScrollGesturesEnabled(true);
+        googleMap.getUiSettings().setRotateGesturesEnabled(false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        googleMap.setMyLocationEnabled(true);
+    }
+
+    private void CheckGPS() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+                .setAlwaysShow(true);
+        Task<LocationSettingsResponse> locationSettingsResponseTask = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+        locationSettingsResponseTask.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(Map_Google.this, "GPS is already enabled", Toast.LENGTH_SHORT).show();
+                    // Request Device Location
+                    GetCurrentUpdate();
+
+                } catch (ApiException e) {
+                    if (e.getStatusCode() == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
+                        ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                        try {
+                            resolvableApiException.startResolutionForResult(Map_Google.this, 101);
+
+                        } catch (IntentSender.SendIntentException sendIntentException) {
+                            sendIntentException.printStackTrace();
+                        }
+
+                    }
+                    if (e.getStatusCode() == LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE) {
+                        Toast.makeText(Map_Google.this, "Settings not available in device", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void GetCurrentUpdate() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(Map_Google.this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Toast.makeText(Map_Google.this, "Location"+ locationResult.getLastLocation().getLongitude()+ ":"+ locationResult.getLastLocation().getLatitude(), Toast.LENGTH_SHORT).show();
+            }
+        }, Looper.getMainLooper());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.noneMap)
+        {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+            return true;
+        }
+        if(item.getItemId()==R.id.NormalMap)
+        {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            return true;
+        }
+        if(item.getItemId()==R.id.SatelliteMap)
+        {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            return true;
+        }
+        if(item.getItemId()==R.id.MapHybrid)
+        {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            return true;
+        }
+        if(item.getItemId()==R.id.MapTerrain)
+        {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                Toast.makeText(this, "Now GPS is Enabled", Toast.LENGTH_SHORT).show();
+            }
+            if(resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
