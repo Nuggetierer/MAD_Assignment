0f6d679da268ffcab3afe10ed8c9c5ad75455585
TehZX <s10221851@connect.np.edu.sg>
2022-07-24 19:51:57 +0800

    upload posts basic stuff

--- app/src/main/java/sg/edu/np/mad/mad_assignment/Posts.java
+++ app/src/main/java/sg/edu/np/mad/mad_assignment/Posts.java
@@ -2,14 +2,39 @@ package sg.edu.np.mad.mad_assignment;
 
 public class Posts {
     private String type;
+    private String title;
     private String caption;
     private Integer review_stars;
+    private String posterID;
     //private (image value type) img_pointer;
 
+    /*
+    post type types
+
+    1) posts - initial submit of a post
+    2) review - child node to a post contains reviews from other users
+
+    */
     public Posts(){};
 
     public String getType() {
         return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPosterID() {
+        return posterID;
+    }
+
+    public void setPosterID(String posterID) {
+        this.posterID = posterID;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public void setType(String type) {
